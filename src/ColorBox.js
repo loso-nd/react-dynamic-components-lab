a517331515f58ce86e8f321d3a8dcf5b018956da
Nahum <losond8@gmail.com>
2021-05-04 20:47:50 -0700

    Refactor conditional statement to ternternary operator. Increased font.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -10,18 +10,25 @@ export default class ColorBox extends Component {
 
   render() {
     console.log(this.props.opacity)
-    if(this.props.opacity >= .2) {
-      return (
-        <div className="color-box" style={{opacity: this.props.opacity /*replace null with the value*/}}>
-          {/* your conditional code here! */
-          <ColorBox opacity={this.props.opacity - 0.1} />
-          }
-        </div>
-      )
-    }
-    else {
-      return null;
-    }
+
+      const newValue = this.props.opacity - 0.1;
+    return this.props.opacity < .2 ? null : (
+      <div className="color-box" style={{opacity: this.props.opacity}}>
+        <ColorBox opacity={newValue}/>
+      </div>
+    )
+    // if(this.props.opacity >= .2) {
+    //   return (
+    //     <div className="color-box" style={{opacity: this.props.opacity}}>
+    //       {/* your conditional code here! */
+    //       <ColorBox opacity={this.props.opacity - 0.1} />
+    //       }
+    //     </div>
+    //   )
+    // }
+    // else {
+    //   return null;
+    // }
 
   }
 
